fade9c6c6bf0577397688ba1ac85c812756b4585
Raul <raul.alhena@gmail.com>
2023-05-11 20:49:57 +0200

    block verification

--- Blockchain.js
+++ Blockchain.js
@@ -19,4 +19,23 @@ export default class Blockchain {
         newBlock.hash = newBlock.calculateHash();
         this.chain.push(newBlock);
     }
+
+    validateChain() {
+        for (let i = 1; i < this.chain.length; i++) {
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i - 1];
+
+            if (currentBlock.hash != currentBlock.calculateHash()) {
+                console.log(`Current Hash: ${currentBlock.hash}, and calculated: ${currentBlock.calculateHash()}`);
+                return false;
+            }
+
+            if (currentBlock.previousHash != previousBlock.hash) {
+                console.log(i)
+                console.log(`Current block previous Hash: ${currentBlock.previousHahs}, and previous block hash: ${previousBlock.hash}`);
+                return false;
+            }
+        }
+        return true;
+    }
 }
--- app.js
+++ app.js
@@ -7,4 +7,6 @@ f5coin.addBlock(new Block("05/04/2023", { amount: 10 }));
 f5coin.addBlock(new Block("08/04/2023", { amount: 50 }));
 f5coin.addBlock(new Block("15/04/2023", { amount: 5 }));
 
-console.log(JSON.stringify(f5coin, null, 4));
+console.log(f5coin.validateChain());
+
+// console.log(JSON.stringify(f5coin, null, 4));
